105d5a2ae23172336a4d5f3c40993079d79257eb
Derek <derekdenhartigh@gmail.com>
2019-05-13 23:38:02 -0400

    escaped some symbols in the text, added some inline styling and swapped the content type from plain text to text/html - probably would have been better to import an html sheet in the response.write, but I'm not sure how/if I can do that.  probably, but I knew this would work and this is just me having fun coding. *end giant commit message*

--- server.js
+++ server.js
@@ -12,9 +12,18 @@ http.createServer((request, response)=>{
     // var queryData = url.parse(request.url, true).query;
     // console.log(queryData,'queryData');
     //write headers
-    response.writeHead(200, {'content-type': "text/plain"})  // format for api return
+    response.writeHead(200, {'content-type': "text/html"})  // format for api return
     // write body
-    response.write(`Random Quote:\n\n${quoteRandomizer(quotes)}`)
+    response.write(`
+    <link href="https://fonts.googleapis.com/css?family=Caveat" rel="stylesheet">
+    <div style="width: 100%; height:100%; display:flex; flex-direction: column; justify-content: center;
+        align-content: center; justify-items: center; align-items: center;
+        font-family: 'Caveat', cursive; font-size: 2em; background-color: #81A0BB;">
+        <h1 style="margin: 0px; padding: 0px; display:flex; color: #602C62">Random Quote:</h1>
+        <br>
+        <p style="margin: 0px; text-align: center; display: flex;  color: #602C62">${quoteRandomizer(quotes)}</p>
+    <div>
+    `)
     response.end(); // close connection
 }).listen(3000); // where to listen
 console.log('running @ http://localhost:3000')
